8ab87a9c72fad65ac2cd24a8e61a7a94577e0ff5
LucasTHz <lucasthomaz58@gmail.com>
2024-04-17 21:19:50 -0300

    chore: manipulation response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,25 +3,33 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
 
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->respond(function (Response $response) {
-            if($response->getStatusCode() === 401) {
-            return response([
-                'message' => 'Não autorizado.',
-            ], 401);
+        $exceptions->respond(function (Response $response, $exceptions) {
+            if ($response->getStatusCode() === 401) {
+                return response([
+                    'message' => 'Não autorizado.',
+                ], 401);
+            }
+
+            if ($exceptions instanceof ValidationException) {
+                return response([
+                    'message' => 'Erro de validação.',
+                    'errors' => $exceptions->errors(),
+                ], 422);
             }
 
             return $response;
