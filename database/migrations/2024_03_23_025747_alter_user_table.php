52a6f4816ad5570676cb619ac99ad2b5d23b7f82
lucasthz <lucasthomaz58@gmail.com>
2025-02-15 10:35:48 -0300

    refactor: update user table to use 'uuid' instead of 'id'

--- database/migrations/2024_03_23_025747_alter_user_table.php
+++ database/migrations/2024_03_23_025747_alter_user_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('document_id', 14)->unique()->nullable();
             $table->date('date_of_birth')->nullable();
-            $table->ulid('id')->change();
+            $table->ulid('uuid')->change();
         });
     }
 
